a28a17ee8ca1c31d4d00b37d4d406a6ba325cfac
Nickothan <s1nicolasrg@gmail.com>
2023-07-06 11:45:15 -0500

    Funciones get all users en controller y service de users.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User, Prisma} from '@prisma/client'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor( private readonly usersService: UsersService) {}
+    
+    @Get()
+    findAll(
+    @Query("page") page = 1,
+    @Query("pagePage") perPage = 10,
+): Promise<User[]> {
+    return this.usersService.findAll(+page, +perPage)
+}
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { User, Prisma} from '@prisma/client'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService){}
+
+    async findAll(page: number, perPage: number): Promise<User[]>{
+        const skip = (page - 1) *perPage
+
+        return this.prisma.user.findMany({
+            skip,
+            take:perPage
+        })
+    }
+}
